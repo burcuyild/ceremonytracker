238c57fd0c6e21b0d41c4cbd52c2db71a369c875
burcuyild <153067669+burcuyild@users.noreply.github.com>
2025-09-07 14:02:13 +0300

    pom.xml

---
+++ src/main/java/tr/com/burcuyildirim/ct/CeremonyTrackerApplication.java
@@ -0,0 +1,12 @@
+package tr.com.burcuyildirim.ct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeremonyTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeremonyTrackerApplication.class, args);
+    }
+}
